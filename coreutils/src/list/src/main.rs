bcabfe9b07e477d6aad315bbd1a5f74b29679208
Chris Zehner <cbzehner@gmail.com>
2019-03-30 16:13:30 -0700

    list: able to list filenames in directory

--- coreutils/src/list/src/main.rs
+++ coreutils/src/list/src/main.rs
@@ -1,3 +1,25 @@
+use std::fs;
+use std::path::PathBuf;
+use structopt::StructOpt;
+
+/// List the contents of a file or directory
+#[derive(StructOpt)]
+struct Arguments {
+    /// The path to list
+    ///
+    /// If no argument is provided, the current directory is listed
+    #[structopt(parse(from_os_str))]
+    path: PathBuf,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Arguments::from_args();
+    let path = args.path;
+    if path.is_dir() {
+        for entry in fs::read_dir(path).unwrap() {
+            let entry = entry.unwrap();
+            let file_name = entry.file_name().into_string().unwrap();
+            println!("{}", file_name);
+        }
+    }
 }
