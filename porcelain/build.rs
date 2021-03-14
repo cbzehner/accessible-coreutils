7f97926c00ca66a2ae2d19c88766134b55a84a82
Chris Zehner <chris@cbzehner.com>
2021-03-13 23:03:49 -0600

    feat: rely on yaml configuration for building wrappers

--- porcelain/build.rs
+++ porcelain/build.rs
@@ -2,16 +2,17 @@ use std::env;
 use std::fs;
 use std::path::Path;
 
-// Read accessible-coreutils.yaml
-// Iterate through the top-level keys and create a binary for each one
-
 fn main() {
+    // TODO:
+    // - Read accessible-coreutils.yaml
+    // - Iterate through the top-level keys and create a binary for each one
+    // - Support multiple languages
     let executable = "list";
+
     // Intentionally bypass the recommendation to rely on OUT_DIR
-    let out_dir = format!("{}/src/bin/", env::var("CARGO_MANIFEST_DIR").unwrap());
-    println!("Output directory is {}", out_dir);
-    println!("Executable is {}", executable);
-    let dest_path = Path::new(&out_dir).join(format!("{}.rs", executable));
+    let project_root = env::var("CARGO_MANIFEST_DIR").unwrap();
+    let dest_path =
+        Path::new(&format!("{}/src/bin/", project_root)).join(format!("{}.rs", executable));
     fs::write(
         &dest_path,
         format!(
@@ -22,11 +23,10 @@ extern crate clap;
 use porcelain::build_executable;
 
 fn main() {{
-    // The YAML file is found relative to the current file, similar to how modules are found
-    let yaml = load_yaml!("../../configs/english/{}.yaml");
+    let yaml = load_yaml!("{}/configs/english/{}.yaml");
     build_executable(yaml);
 }}"###,
-            executable
+            project_root, executable
         ),
     )
     .unwrap();
--- porcelain/src/lib.rs
+++ porcelain/src/lib.rs
@@ -18,13 +18,15 @@ const FAILURE_UNKNOWN: i32 = -1;
 // }
 
 pub fn build_executable(yaml: &Yaml) {
-    let matches = App::from_yaml(yaml)
+    // Build the wrapper based on the YAML configuration.
+    let matches = App::from_yaml(&yaml)
         // Global arguments available in all binaries
         .args(&[
-            Arg::with_name("verbose")
-                .short("v")
-                .multiple(true)
-                .help("Sets the level of verbosity"),
+            // TODO: Set a verbosity level.
+            // Arg::with_name("verbose")
+            //     .short("v")
+            //     .multiple(true)
+            //     .help("Sets the level of verbosity"),
             Arg::with_name("version")
                 .long("version")
                 .help("Show the current version"),
@@ -33,17 +35,41 @@ pub fn build_executable(yaml: &Yaml) {
         .version(crate_version!())
         .get_matches();
 
-    // Globally present options
-    // - version
-    // - verbosity
-    // - handle colorization based on whether it's part of a pipe or not
-    // - ???
+    // Print the version and exit when the version flag is requested.
     if matches.is_present("version") {
         println!("{}", crate_version!());
         process::exit(0)
     }
 
-    match command("ls", vec!["-l", "-a", "."]) {
+    // TODO: If the command is run by a human, include the humanize flags.
+
+    // Get the name of the underlying binary.
+    let command_name = yaml["executable"].as_str().unwrap();
+    // Initialize a vector to hold arguments to pass through to the underlying binary.
+    let mut command_args: Vec<&str> = vec![];
+
+    // Iterate through the args from the YAML configuration.
+    let wrapper_args = yaml["args"].as_vec().unwrap();
+    for arg in wrapper_args {
+        for (k, v) in arg.as_hash().unwrap() {
+            let key = k.as_str().unwrap();
+            // If the argument is present in the current invocation...
+            if matches.is_present(key) {
+                // If a map is present on the config, push it to the list of args for the wrapped command.
+                if !&v["map"].is_badvalue() {
+                    command_args.push(&v["map"].as_str().unwrap())
+                }
+
+                // If a value is present in the wrapper, push it to the list of args for the wrapped command.
+                if let Some(value) = matches.value_of(key) {
+                    command_args.push(value)
+                }
+            }
+        }
+    }
+
+    // Call the wrapped command by name, passing in any relevant arguments.
+    match command(command_name, command_args) {
         Ok(success) => process::exit(success),
         Err(message) => {
             eprintln!("Error: {}", message);
@@ -52,8 +78,8 @@ pub fn build_executable(yaml: &Yaml) {
     }
 }
 
-fn command(binary_name: &str, arguments: Vec<&str>) -> Result<i32, String> {
-    let child = process::Command::new(binary_name)
+fn command(name: &str, arguments: Vec<&str>) -> Result<i32, String> {
+    let child = process::Command::new(name)
         .args(&arguments)
         .spawn()
         .map_err(|error| error.to_string())?;
